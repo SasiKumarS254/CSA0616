8864030c9355bbd675a084d4feb094a9e870fe48
sasikumar <sasikumars4254.sse@saveetha.com>
2024-11-20 19:57:33 +0530

    Update Fibonacci sequences.py

--- Fibonacci sequences.py
+++ Fibonacci sequences.py
@@ -3,4 +3,4 @@ a,b,fib_sum=0,1,0
 for i in range(terms):
     fib_sum+=a
     a,b=b,b+a
-print(f"THE SUM OF THE FIBANOCCI SEQUENCES IS {fib_sum}")
+print(f"THE SUM OF THE FIONACCI SEQUENCES IS {fib_sum}")
